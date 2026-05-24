6c31948de54c160a7b75c9d1fbb43221771c6e0e
Bosshub <bosshubofficial96@gmail.com>
2026-05-24 13:57:33 +0530

    Create index.ts

---
+++ backend-node/src/modules/auth/strategies/index.ts
@@ -0,0 +1,28 @@
+/**
+ * AnimeMultiFlix - Strategies Index
+ * Version: 4.0.0 (2026)
+ */
+
+export { JwtStrategy } from './jwt.strategy';
+export { GoogleStrategy } from './google.strategy';
+export { DiscordStrategy } from './discord.strategy';
+export { FacebookStrategy } from './facebook.strategy';
+export { AppleStrategy } from './apple.strategy';
+export { GitHubStrategy } from './github.strategy';
+export { TwitterStrategy } from './twitter.strategy';
+export { LinkedInStrategy } from './linkedin.strategy';
+export { TwitchStrategy } from './twitch.strategy';
+export { RedditStrategy } from './reddit.strategy';
+
+export default {
+    JwtStrategy,
+    GoogleStrategy,
+    DiscordStrategy,
+    FacebookStrategy,
+    AppleStrategy,
+    GitHubStrategy,
+    TwitterStrategy,
+    LinkedInStrategy,
+    TwitchStrategy,
+    RedditStrategy
+};
